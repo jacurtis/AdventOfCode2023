d9d47df149b20eee7aaf82d0ee95f2b30c459765
Alex Curtis <jacurtis@hey.com>
2023-12-02 23:36:28 -0700

    feat: Completed Day 1 Challenge 2

--- day1/day1.go
+++ day1/day1.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func sumSlice(arr []int) (sum int) {
@@ -15,7 +16,36 @@ func sumSlice(arr []int) (sum int) {
 	return sum
 }
 
-func main() {
+func convStrNumber(str string) string {
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		switch str {
+		case "one":
+			num = 1
+		case "two":
+			num = 2
+		case "three":
+			num = 3
+		case "four":
+			num = 4
+		case "five":
+			num = 5
+		case "six":
+			num = 6
+		case "seven":
+			num = 7
+		case "eight":
+			num = 8
+		case "nine":
+			num = 9
+		}
+	}
+	representation := strconv.Itoa(num)
+	return representation
+
+}
+
+func part1(debug ...bool) {
 	fmt.Println("Day 1 has arrived!")
 	var inputs []string
 	var results []int
@@ -71,4 +101,78 @@ func main() {
 
 	// Sum all values in the array
 	fmt.Printf("\nSum: %d\n", sumSlice(results))
+}
+
+func part2(debug ...bool) {
+	fmt.Println("Day 1 - Part 2")
+	if len(debug) == 0 {
+		debug = append(debug, false)
+	}
+	var inputs []string
+	var results []int
+
+	// Load input value
+	// Separate lines into array of strings
+	file, err := os.Open("day1/input.txt")
+	if err != nil {
+		fmt.Println("Error opening input file")
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		inputs = append(inputs, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading input file")
+		return
+	}
+
+	if debug[0] {
+		fmt.Printf("Inputs: %d\n", len(inputs))
+	}
+
+	// Loop through each string
+	// -- Find the first digit from the front
+	// -- Find the last digit from the back
+	// -- Concatenate them together as int
+	r, err := regexp.Compile(`\d|one|two|three|four|five|six|seven|eight|nine`)
+	if err != nil {
+		fmt.Println("Error compiling regex for forward digit search")
+		return
+	}
+
+	for index, input := range inputs {
+		if debug[0] {
+			fmt.Printf("Input %d: %s\n", index+1, input)
+		}
+		//matches := r.FindAllString(input, -1)
+		var matches []string
+		for i := range input {
+			match := r.FindString(input[i:])
+			if match != "" {
+				matches = append(matches, match)
+			}
+		}
+		resultInt, err := strconv.Atoi(convStrNumber(matches[0]) + convStrNumber(matches[len(matches)-1]))
+		if err != nil {
+			fmt.Println("Error converting resulting string to int")
+			return
+		}
+		if debug[0] {
+			fmt.Printf("Result: %d\n", resultInt)
+		}
+		results = append(results, resultInt)
+	}
+
+	// Sum all values in the array
+	fmt.Println(strings.Repeat("^", 20))
+	fmt.Printf("Sum: %d\n", sumSlice(results))
+}
+
+func main() {
+	//part1()
+	part2()
 }
